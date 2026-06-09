bf7fc042a1c5eac56e2cdcd77d7b5fdf13a9c007
rohit-rai-0101rm <rohitrai.dev@gmail.com>
2026-06-09 08:06:58 +0530

    feat:text splitted into chunks

--- rag_agent/index.py
+++ rag_agent/index.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 pdf_path=Path(__file__).parent / "node js.pdf"
 
@@ -9,4 +10,9 @@ loader=PyPDFLoader(file_path=pdf_path)
 docs=loader.load()
 
 
-print(docs[12])
+text_splitter=RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=400
+)
+
+chunks=text_splitter.split_documents(documents=docs)
